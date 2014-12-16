deed3e833fd99bafce34a6ce3b6be72b07e90938
Isildy <isildy@mail.ru>
2014-12-16 15:51:31 +0200

    pars JSON

--- homework-3/Profile.rb
+++ homework-3/Profile.rb
@@ -1,39 +1,39 @@
 require 'json'
 module AutoProfile
-	module GenMethods
-		def gen_methods(methods)
-			methods.each do |key, value| 
-			define_method key do
-			  p "#{key}:"
-			if value.is_a?(Hash)
-				value.each{|key, value| p "      #{key}: " 
-					if value.is_a?(Hash)
-				value.each{|key, value| p "    #{key} - #{value}"}						
-					elsif	
-				value.is_a?(Array)
-				value.each{|key| 
-					if key.is_a?(Hash)
-					key.each{|key, value| p "        #{key} - #{value}"}
-						else
-				 			p "        #{key}"
-						end}
-					else
-						p "           #{value}"
-					end}
-			elsif	
-				value.is_a?(Array)
-				value.each{|key| p "      #{key}"} 
-			else
-				p "#{value}"	
-				end
-			end
-		  end
-		end
+  module GenMethods
+	def gen_methods(methods)
+	  methods.each do |key, value| 
+		define_method key do
+		  p "#{key}:"
+		  if value.is_a?(Hash)
+			value.each{|key, value| p "      #{key}: " 
+			  if value.is_a?(Hash)
+			  value.each{|key, value| p "    #{key} - #{value}"}						
+			  elsif	
+			   value.is_a?(Array)
+			   value.each{|key| 
+				  if key.is_a?(Hash)
+				  key.each{|key, value| p "        #{key} - #{value}"}
+			   	  else
+				  p "        #{key}"
+				  end}
+				else
+				  p "           #{value}"
+			end}
+		  elsif	
+			value.is_a?(Array)
+			value.each{|key| p "      #{key}"} 
+		  else
+			p "#{value}"	
+	      end
+	  end
+	end
+  end
 end
 
-	def self.included(base)
-		base.extend(GenMethods)
-	end
+  def self.included(base)
+	base.extend(GenMethods)
+  end
 end
 
 
@@ -44,46 +44,45 @@ end
 												}}}'	
 
 response = JSON.parse(RESPONSE)
-	if response.key?("person")
+  if response.key?("person")
 
-		person_object = Struct.new("Person", *response["person"].keys.collect(&:to_sym))
+	person_object = Struct.new("Person", *response["person"].keys.collect(&:to_sym))
 
-		person = person_object.new(*response["person"].values)
-	end
+	person = person_object.new(*response["person"].values)
+  end
 
 class PrintJSONinfo
-def	adult?(data)
-	    if data["personal_data"]["age"] < 15 
-			p "To yang"
-		elsif data["personal_data"]["age"] > 60
-			p "To old"
-		else
-			p "Age is #{data["personal_data"]["age"]}, evesing Ok"
-		end	
-	end
+  def	adult?(data)
+    if data["personal_data"]["age"] < 15 
+	 p "To yang"
+    elsif data["personal_data"]["age"] > 60
+	 p "To old"
+    else
+	 p "Age is #{data["personal_data"]["age"]}, evesing Ok"
+    end	
+  end
 
-	def twitter_account? (data)
-		twiter_acc = "no"
-		data["social_profiles"].each{|x| if x=~(/twitter\/\w+/) then twiter_acc = x end} 
-		if 
-			twiter_acc == "no"
-			p "#{data["personal_data"]["name"]} has no twitter account"
-		else
-			p "Twiter account - #{twiter_acc}"
-		end
-	end
+  def twitter_account? (data)
+    twiter_acc = "no"
+	data["social_profiles"].each{|x| if x=~(/twitter\/\w+/) then twiter_acc = x end} 
+	if 
+      twiter_acc == "no"
+      p "#{data["personal_data"]["name"]} has no twitter account"
+	else
+	  p "Twiter account - #{twiter_acc}"
+    end
+  end
 
-	def have_hobbies?(data)
-		hobby = "no"
-		data["additional_info"].each{|x| Hash[*x].each_pair{|key, value| if key == "hobby" then hobby=value end}}			
-		if 
-			hobby == "no"
-			p "#{data["personal_data"]["name"]} has no hobby"
-		else
-			p "#{data["personal_data"]["name"]} likes #{hobby.join(", ")}"
-		end
-	end
-
+  def have_hobbies?(data)
+	hobby = "no"
+	data["additional_info"].each{|x| Hash[*x].each_pair{|key, value| if key == "hobby" then hobby=value end}}			
+	 if 
+	  hobby == "no"
+	  p "#{data["personal_data"]["name"]} has no hobby"
+	 else
+      p "#{data["personal_data"]["name"]} likes #{hobby.join(", ")}"
+	 end
+  end
 end
 
 PrintJSONinfo.class_eval do
