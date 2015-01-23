439704cee9e35156270802978fb46f484069b213
Isildy <isildy@mail.ru>
2015-01-23 12:20:42 +0200

    start homework(tests)

--- homework-5/Gladiator.rb
+++ homework-5/Gladiator.rb
@@ -1,32 +1,5 @@
 require 'json'
- module Actions
-  module GladiatorsActions
-	def generate_actions(data, actions)		
-	  data.each do |k, v| 
-		if v.is_a?(Hash)
-		   v.each do |k, v| 
-			if k == "name"
-		   define_method ("greeting") do
-		    p "Going to death #{v}, salute you!"
-		      end
-	   		elsif k == "weapon"
-	   			v.each do |v|
-	       		 	actions.each_pair do|action, way| 
-	       		define_method ("#{action}_vis_#{v}") do
-	       						way.call(v)
-	       	  end
-	       	  end
-	       	  end
-	     	end
-	  	   end
-	    end
-	  end
-    end
-  end
-  def self.included(base)
-	base.extend(GladiatorsActions)
-  end
-end
+
 
 RESPONSE = '{"gladiator":{"personal_data":{"name": "Staros", "gender":"male", "age":27},
 							  "skills":["swordsman","spearman","bowmen"],
@@ -53,33 +26,44 @@ RESPONSE = '{"gladiator":{"personal_data":{"name": "Staros", "gender":"male", "a
 	weapon = "no"
 	amunition.each{|x| Hash[*x].each_pair{|key, value| if key == "weapon" then weapon=value end}}			
      p "Glagiator #{personal_data["name"]} is carrying a #{weapon.join(", ")}"	 
-   end
- end
+  end
 
+	def generate_actions(data, actions)		
+	  data.each do |k, v| 
+		if v.is_a?(Hash)
+		   v.each do |k, v| 
+			if k == "name"
+		   define_singleton_method ("greeting") do
+		    p "Going to death #{v}, salute you!"
+		      end
+	   		elsif k == "weapon"
+	   			v.each do |v|
+	       		 	actions.each_pair do|action, way| 
+	       		define_singleton_method ("#{action}_vis_#{v}") do
+	       						way.call(v)
+	       	  end
+	       	  end
+	       	  end
+	     	end
+	  	   end
+	    end
+	  end
+    end
 
-Gladiator.class_eval do
-	include Actions
-	
-	trident_actions = {:attack => Proc.new{ |weapon| p "Strike #{weapon} in face"}, 
-					   :defance => Proc.new{ |weapon| p "Block #{weapon} weapon of the opponent"},
-					   :fint => Proc.new{ |weapon| p "Apply #{weapon} feint"} 
-						}
-
-	#tridentAct = lambda do
-	       	#define_method ("trident_#{action}") do
-	  #     	p "trident_{action}:{way}" end 	
-	       	#end
- 
-	generate_actions(response["gladiator"], trident_actions)
 end
 
 
-
+trident_actions = {:attack => Proc.new{ |weapon| p "Strike #{weapon} in face"}, 
+					:defance => Proc.new{ |weapon| p "Block #{weapon} weapon of the opponent"},
+					 :fint => Proc.new{ |weapon| p "Apply #{weapon} feint"} 
+						}
 					
 
 person = Gladiator.new(*response["gladiator"].values)
+person.generate_actions(response["gladiator"], trident_actions)
 
-
+print "\n"
+p 
 p person.public_methods(false)
 print "\n"
 print "\n"
@@ -95,4 +79,3 @@ p "======== Lets fight!!!!!!!!!!!!!!!! ========"
 person.fint_vis_trident
 person.defance_vis_net
 person.attack_vis_dagger
-
