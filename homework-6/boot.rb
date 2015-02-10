3cf6f15e9e309c8476565e00da1acd2997ebeb7c
Isildy <isildy@mail.ru>
2015-02-10 12:50:36 +0200

    add views

--- homework-6/boot.rb
+++ homework-6/boot.rb
@@ -12,6 +12,12 @@ require 'kaminari'
 register Kaminari::Helpers::SinatraHelpers
 register Padrino::Helpers
 
+
+#set :environment, :production
+enable :sessions
+enable :logging
+enable :protect_from_csrf
+
 require "./model"
 
 get "/" do
@@ -20,37 +26,25 @@ end
 
 get "/gladiators" do
   glads = Gladiator.all
-  erb :index , locals:{glads: glads}
+  erb :index , locals:{glads: glads}, layout: "layout"
 end
 
 get "/gladiators/new" do
-	@glad = Gladiator.new
-	@glad.name = "Sparos"
-	@glad.age = 27
-	@glad.gender = "mele"
-
-	@weapon = Weapon.new
-
-	@amunition = Amunition.new
-	@amunition.title = "gladius"
-	@amunition.description = "short sword"
-	@amunition.resource = @weapon
-	@glad.amunitions << @amunition
-	@glad.saveredirect to("/gladiators")
-	erb :new , locals:{glad: glad}
+	glad = Gladiator.new
+	erb :new , locals:{glad: glad}, layout: "layout"
 end
 
 get "/gladiators/:id/edit" do |id|
 	glad = Gladiator.find(id)
-	erb :edit , locals:{glad: glad}
+	erb :edit , locals:{glad: glad}, layout: "layout"
 end
 
 post "/gladiators" do
 	glad = Gladiator.new(params[:gladiator])
 	if glad.save
-	redirect to("/services/#{glad.id}")
+	redirect to("/gladiators/#{glad.id}")
 	else
-	erb :new, locals: {glad: glad}
+	erb :new, locals: {glad: glad}, layout: "layout"
 	end
 end
 
@@ -60,7 +54,7 @@ put "/gladiators/:id" do |id|
 	if glad.save
 		redirect to("/gladiators/#{glad.id}")
 	else
-		erb :edit, locals: {glad: glad}
+		erb :edit, locals: {glad: glad}, layout: "layout"
 	end
 end
 
@@ -70,11 +64,29 @@ delete "/gladiators/:id" do |id|
 	redirect to("/gladiators")
 end
 
-
-
-get '/weapon/:id' do |id|
+get '/gladiators/:id' do |id|
 
   glad = Gladiator.find(id.to_i)
-  erb :weapon , locals:{glad: glad}
+  erb :show , locals:{glad: glad}, layout: "layout"
 
 end 
+
+get "/gladiators/:id/weapon/new" do |id|
+	glad = Gladiator.find(id)
+	amunition = Amunition.new
+	erb :weapon , locals:{glad: glad, amunition: amunition}, layout: "layout"
+end
+
+|id|
+post "/gladiators/:id/weapon/new" do |id|
+	glad = Gladiator.find(id)
+	amunition = Amunitions.new(params[:gladiator])
+	glad.amunitions << amunition
+	if glad.save
+	redirect to("/gladiators/")
+	else
+	erb :weapon, locals: {glad: glad, amunition: amunition }, layout: "layout"
+	end
+end
+
+
